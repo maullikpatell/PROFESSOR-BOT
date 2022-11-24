7fa8db761e1cbc08e857ad1e6896d8862b61ad6e
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-24 18:50:05 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -57,8 +57,7 @@ async def save_file(media):
             logger.warning(media.file_name + " is already saved in database")
             return False, 0
         else:
-            logger.info(getattr(media, file_name, "NO FILE NAME") + "is saved in database")
-
+            logger.info(getattr(media, "file_name", "NO FILE NAME") + "is saved in database")
             return True, 1
 
 
